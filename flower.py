b2412524c1f7a056836702b6d6a11bb28344532a
siwi <771089204@qq.com>
2026-06-07 10:48:53 +0800

    Update flower.py

--- flower.py
+++ flower.py
@@ -4,7 +4,7 @@ import pathlib
 # 1. 下载花数据集
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
 data_dir = tf.keras.utils.get_file("flower_photos", origin=dataset_url, untar=True)
-data_dir = pathlib.Path(data_dir)
+data_dir = pathlib.Path(data_dir) / "flower_photos"
 
 # 2. 参数
 batch_size = 32
