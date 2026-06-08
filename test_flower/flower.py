a362612d2124ff79bb968f01809236fdc8b51ba6
siwi <771089204@qq.com>
2026-06-08 19:43:54 +0800

    Update flower.py

--- test_flower/flower.py
+++ test_flower/flower.py
@@ -1,38 +1,74 @@
-import tensorflow as tf
-import pathlib
+import tensorflow as tf # the main libraries to achieve recognition
+import pathlib # easier to code the pathway
 
-# 1. 下载花数据集
-dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-data_dir = tf.keras.utils.get_file("flower_photos", origin=dataset_url, untar=True)
-data_dir = pathlib.Path(data_dir) / "flower_photos"  # 加这一行
+# 1. Download flower dataset
 
-# 2. 参数
-batch_size = 32
-img_size = (180, 180)
+# source of the flower dataset
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz" 
 
-# 3. 读取数据（关键函数）
+# download dataset
+data_dir = tf.keras.utils.get_file("flower_photos", origin=dataset_url, untar=True) 
+
+# save the dataset into five categories
+data_dir = pathlib.Path(data_dir) / "flower_photos"  
+
+# 2. Parameter
+
+# feed 32 images at once
+batch_size = 32 
+
+# edit the size of images into the same
+img_size = (180, 180) 
+
+# 3. Read data
+
+# training dataset
 train_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
+    
+    # import dataset
+    data_dir, 
+    
+    # the ratio of training subset and validation subset is 8:2
+    validation_split=0.2, 
+    
+    # read training subset because it's a train_ds
     subset="training",
-    seed=123,
+    
+    # a fixed seed which can generate the same random sequence all the time, so it can make sure that the model always gets same results
+    seed=123, 
+    
+    # import image_size
     image_size=img_size,
+    
+    # import batch_size
     batch_size=batch_size,
 )
 
+# validation dataset, most of the parametres are the same
 val_ds = tf.keras.utils.image_dataset_from_directory(
     data_dir,
     validation_split=0.2,
-    subset="validation",
+    # read validation subset because it's a val_ds
+    subset="validation", 
     seed=123,
     image_size=img_size,
     batch_size=batch_size,
 )
 
-# 4. 性能优化
+# 4. Perfeomance optimization
+
+# TensorFlow would choose the optimal numbers of progresses automatically based on the performance of CPU, GPU and Memory
 AUTOTUNE = tf.data.AUTOTUNE
+
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+"""
+cache the dataset
+shuffle 1000 images first and pick randomly from it to improve model's performance and avoid overfitting
+prefetch let CPU and GPU work at the same time, so there is no need for GPU to wait
+"""
+
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+#there is no need to shuffle because order does not affect the accuracy
 
 # 5. 建模型（简单CNN）
 model = tf.keras.Sequential(
