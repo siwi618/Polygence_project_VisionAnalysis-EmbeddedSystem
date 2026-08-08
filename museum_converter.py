539f96241570a49928fac68a24936f99fe7cc97f
siwi <771089204@qq.com>
2026-08-08 08:07:31 +0800

    Update museum_converter.py

--- museum_converter.py
+++ museum_converter.py
@@ -5,7 +5,11 @@ base_dir = Path(__file__).resolve().parent
 
 model = tf.keras.models.load_model(base_dir / "museum_model.keras")
 
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
+
+# Pin batch=1 so TFLite input is [1, H, W, 3] (not frozen batch=4)
+concrete = tf.function(lambda x: model(x, training=False))
+concrete = concrete.get_concrete_function(tf.TensorSpec([1, 180, 180, 3], tf.float32))
+converter = tf.lite.TFLiteConverter.from_concrete_functions([concrete], model)
 
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 converter.target_spec.supported_types = [tf.float16]
@@ -14,3 +18,5 @@ tflite_model = converter.convert()
 
 with open(base_dir / "museum_fp16.tflite", "wb") as f:
     f.write(tflite_model)
+
+print("Saved museum_fp16.tflite")
